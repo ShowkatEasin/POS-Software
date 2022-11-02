5b47dcc282da3bd45c7adb92c6a69ce910e05e09
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-11-02 21:31:47 +0600

    Done

---
+++ blank.php
@@ -0,0 +1,69 @@
+<?php
+include "includes/header.php";
+
+?>
+
+  <!-- Preloader -->
+  <?php
+  include "includes/loader.php";
+?>
+  <!-- Navbar -->
+ 
+  <?php
+  include "includes/navbar.php";
+  ?>
+ <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <?php
+  include "includes/mainslider.php";
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <?php
+    include "includes/headercontent.php";
+?>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-8">
+
+          <h1>My content </h1>
+
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!--/. container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <?php
+  include "includes/footer.php";
+  ?>
+</div>
+<!-- ./wrapper -->
+
+
+<!-- REQUIRED SCRIPTS -->
+
+<?php
+  include "includes/scripts.php";
+  ?>
+
+
+</body>
+</html>
